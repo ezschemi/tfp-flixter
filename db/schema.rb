187fb9bfa0e6502b9407f2f7c33baab68aed4776
Enrico Zschemisch <enrico@vicampus.com>
2016-11-17 04:26:47 -0500

    added individual course sections: their creation and listing

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161117064814) do
+ActiveRecord::Schema.define(version: 20161117091144) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "course_sections", force: :cascade do |t|
+    t.string   "title"
+    t.integer  "course_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["course_id"], name: "index_course_sections_on_course_id", using: :btree
+  end
 
   create_table "courses", force: :cascade do |t|
     t.string   "title"
@@ -23,6 +31,14 @@ ActiveRecord::Schema.define(version: 20161117064814) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.index ["user_id"], name: "index_courses_on_user_id", using: :btree
+  end
+
+  create_table "sections", force: :cascade do |t|
+    t.string   "title"
+    t.integer  "course_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["course_id"], name: "index_sections_on_course_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
