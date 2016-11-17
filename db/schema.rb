cceb80681f35029ed274297374ad075c8b9a6178
Enrico Zschemisch <enrico@vicampus.com>
2016-11-17 04:51:34 -0500

    added lessons model and forms

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161117091144) do
+ActiveRecord::Schema.define(version: 20161117092826) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,15 @@ ActiveRecord::Schema.define(version: 20161117091144) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.index ["user_id"], name: "index_courses_on_user_id", using: :btree
+  end
+
+  create_table "lessons", force: :cascade do |t|
+    t.string   "title"
+    t.string   "subtitle"
+    t.integer  "section_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["section_id"], name: "index_lessons_on_section_id", using: :btree
   end
 
   create_table "sections", force: :cascade do |t|
