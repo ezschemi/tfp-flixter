3c7dedf1f2c220238a9025b6d18a62a360095b1c
Enrico Zschemisch <enrico@vicampus.com>
2016-11-17 07:37:06 -0500

    Added course-image feature with carrierwave upload

--- app/models/course.rb
+++ app/models/course.rb
@@ -5,4 +5,6 @@ class Course < ApplicationRecord
   validates :title, presence: true
   validates :description, presence: true
   validates :cost, presence: true, numericality: { greater_than: 0 }
+
+  mount_uploader :image, ImageUploader
 end
