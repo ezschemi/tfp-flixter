7b8bad52e8034f835a307c4a1475d327bb841ee8
Enrico Zschemisch <enrico@vicampus.com>
2016-11-17 08:04:06 -0500

    switched from fog to carrierwave-aws gem due to performance reasons

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,11 @@
 CarrierWave.configure do |c|
-  c.fog_credentials = {
-    :provider   => "AWS",
-    :aws_access_key_id => ENV["AWS_ACCESS_KEY"],
-    :aws_secret_access_key => ENV["AWS_SECRET_KEY"],
+  c.storage = :aws
+  c.aws_bucket = ENV["AWS_BUCKET"]
+  c.aws_acl = "public_read"
+
+  c.aws_credentials = {
+    access_key_id: ENV["AWS_ACCESS_KEY"],
+    secret_access_key: ENV["AWS_SECRET_KEY"],
+    region: ENV["AWS_REGION"]
   }
-  c.fog_directory = ENV["AWS_BUCKET"]
 end
