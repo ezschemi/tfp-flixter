2bccf9407abaa9dc0f91aafefe2e0e71a2c4197e
Enrico Zschemisch <enrico@vicampus.com>
2016-11-18 02:27:19 -0500

    removed erronous characters and duplicated code from lessons_controller

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -4,42 +4,11 @@ class LessonsController < ApplicationController
 
 
   def show
-    # current_user available now
-    userID = current_user.id 
-    courseID = current_lesson.section.course.id
-
-    @valid = false
-    current_lesson.section.course.enrollments.each do |enrollment|
-      if enrollment.user_id == @userID && enrollment.course_id == @courseID
-        @valid = true 
-        break
-      end
-    end
-    if !@valid
-      redirect_to course_path(current_lesson.section.course)_
-    end
   end
 
   private
 
   def require_authorized_for_current_user_1
-    #if current_course.user != current_user
-    #  render text: "Unauthorized", status: :unauthorized
-    #end
-    userID = current_user.id 
-    courseID = current_lesson.section.course.id
-
-    valid = false
-    current_lesson.section.course.enrollments.each do |enrollment|
-      if enrollment.user_id == userID && enrollment.course_id == courseID
-        valid = true
-        break
-      end
-    end
-
-    if !valid
-      render text: "Not enrolled", status: :unauthorized
-    end
   end
 
   def require_authorized_for_current_user
